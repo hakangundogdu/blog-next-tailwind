8314b5bd8cd8919d66da561419bf67b8216e4f31
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-05-29 13:05:50 +0300

    Banner.

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,13 +1,13 @@
 const Banner = () => {
   return (
     <div className="max-w-8xl container mx-auto flex items-center justify-center ">
-      <div className="mt-6 flex w-full items-center justify-between rounded-xl border p-8 text-left hover:border-slate-300 hover:shadow-md lg:w-1/2 ">
-        <h2 className="text-2xl font-extrabold text-teal-500">
+      <div className="mt-6 flex w-full items-center justify-between rounded-xl border bg-teal-500 p-10 text-left shadow hover:border-slate-300 lg:w-1/2 ">
+        <h2 className="text-2xl font-extrabold text-white">
           {' '}
           Ready to dive in? <br />
           <span className="text-slate-800 "> Star your trial today!</span>
         </h2>
-        <button className="h-10 rounded-md border border-cyan-300 bg-slate-100 bg-gradient-to-r from-teal-500 to-cyan-500 px-6 font-semibold text-white">
+        <button className="h-10 rounded-md bg-slate-100 px-6 font-semibold text-teal-500 shadow hover:bg-teal-50 hover:text-teal-800 ">
           Get Started
         </button>
       </div>
